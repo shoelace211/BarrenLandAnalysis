0ec7be864fa13bd5811786fc4eabb22a491d1232
Ashley Schuelein <aschuelein@comcast.net>
2018-10-20 18:13:14 -0500

    Basic test in main working

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,12 @@ public class Main {
     public static Land myLand;
 
     public static void main(String[] args) {
+        String input = new String("{0 292 399 307}");
         myLand = new Land();
-
-
+        myLand.readInput(input);
+        myLand.clearBarren();
+        myLand.setBarren();
+        myLand.calcFertileLand();
+        System.out.println(myLand.printResults());
     }
-
 }
